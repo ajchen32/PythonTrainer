f2e3e77d698390d7224c1c03171d7541e773b052
ajchen32 <ajchen32@gmail.com>
2025-12-26 17:04:57 -0600

    Added template

--- backend/Problems/Problem1.py
+++ backend/Problems/Problem1.py
@@ -1,1 +1,19 @@
 # Add all numbers from 1 to n and return the sum
+
+# output instructions function
+def instructions():
+    pass 
+
+def checkUserCode(userInput):
+    pass
+
+# this should have problems and their solutions
+def makeTestCases(): 
+    pass
+
+# whatever you want
+def feedback():
+    pass
+
+
+    
